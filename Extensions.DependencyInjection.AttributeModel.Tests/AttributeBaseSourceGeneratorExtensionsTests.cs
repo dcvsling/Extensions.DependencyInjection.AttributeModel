bfd2b63849a325b38b714da69f3514a2f3905407
dcvsling <dcvsling@dcvs.idv.tw>
2022-04-20 09:48:19 +0800

    reject second invoke AddAttributeModelRegister

---
+++ Extensions.DependencyInjection.AttributeModel.Tests/AttributeBaseSourceGeneratorExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyInjection.Tests;
+public class AttributeModelServiceCollectionTests
+{
+    [Fact]
+    public void Invoke_AddAttributeModelRegister_twice_should_register_once()
+    {
+        var collection = new ServiceCollection()
+            .AddAttributeModelRegister()
+            .AddAttributeModelRegister();
+
+        Assert.Single(collection.Where(x => x.ServiceType == typeof(TestService)));
+    }
+}
+
+[Singleton]
+public class TestService { }
--- Extensions.DependencyInjection.AttributeModel/AttributeBaseSourceGeneratorExtensions.cs
+++ Extensions.DependencyInjection.AttributeModel/AttributeBaseSourceGeneratorExtensions.cs
@@ -6,8 +6,21 @@ namespace Microsoft.Extensions.DependencyInjection;
 public static class AttributeBaseSourceGeneratorExtensions
 {
     public static IServiceCollection AddAttributeModelRegister(this IServiceCollection services)
-        => AppDomain.CurrentDomain.GetAssemblies()
+    {
+        if (services.Any(x => x.ImplementationInstance?.Equals(Markup.Instance) ?? false))
+            return services;
+        AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(x => x.GetCustomAttributes())
             .OfType<IDesignTimeServiceCollectionConfiguration>()
             .Aggregate(services, (srv, config) => config.ConfigureService(srv));
+        services.Insert(0, ServiceDescriptor.Singleton(Markup.Instance));
+        return services;
+    }
+
+    private class Markup
+    {
+        private Markup() { }
+        public static Markup Instance { get; } = new Markup();
+    }
 }
+
