ebe3a5bf3f96bae2aee84973b52d72af14e955a3
dcvsling <dcvsling@dcvs.idv.tw>
2022-06-18 14:32:37 +0800

    reject second invoke AddAttributeModelRegister

--- Extensions.DependencyInjection.AttributeModel/AttributeBaseSourceGeneratorExtensions.cs
+++ Extensions.DependencyInjection.AttributeModel/AttributeBaseSourceGeneratorExtensions.cs
@@ -7,6 +7,8 @@ public static class AttributeBaseSourceGeneratorExtensions
 {
     public static IServiceCollection AddAttributeModelRegister(this IServiceCollection services)
     {
+        if (services.Any(x => x.ImplementationInstance?.Equals(Markup.Instance) ?? false))
+            return services;
         var configs = AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(x => x.GetCustomAttributes())
             .OfType<IDesignTimeServiceCollectionConfiguration>()
@@ -15,6 +17,14 @@ public static class AttributeBaseSourceGeneratorExtensions
         configs.Aggregate(services, (srv, config) => config.ConfigureService(srv));
         configs.Aggregate(services, (srv, config) => config.ConfigureDecorator(srv));
 
+        services.Insert(0, ServiceDescriptor.Singleton(Markup.Instance));
         return services;
     }        
+
+    private class Markup
+    {
+        private Markup() { }
+        public static Markup Instance { get; } = new Markup();
+    }
 }
+
